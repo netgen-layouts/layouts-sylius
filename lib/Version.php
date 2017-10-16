a8ef6a651d08e235c220c1cd271aa8a523b625e5
Edi Modrić <edi.modric@gmail.com>
2017-10-16 10:45:20 +0200

    Bump version to 0.9.1-DEV

--- lib/Version.php
+++ lib/Version.php
@@ -4,10 +4,10 @@ namespace Netgen\BlockManager\Sylius;
 
 abstract class Version
 {
-    const VERSION = '0.9.0-DEV';
-    const VERSION_ID = 900;
+    const VERSION = '0.9.1-DEV';
+    const VERSION_ID = 901;
     const MAJOR_VERSION = 0;
     const MINOR_VERSION = 9;
-    const RELEASE_VERSION = 0;
+    const RELEASE_VERSION = 1;
     const EXTRA_VERSION = 'DEV';
 }
