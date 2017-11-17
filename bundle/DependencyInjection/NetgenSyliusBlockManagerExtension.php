c7430ae064135d3b96f34898eebd81979bdb8f31
Edi Modrić <edi.modric@gmail.com>
2017-11-17 10:30:04 +0100

    Add sylius design and basic semanticui templates for grid, list and containers

--- bundle/DependencyInjection/NetgenSyliusBlockManagerExtension.php
+++ bundle/DependencyInjection/NetgenSyliusBlockManagerExtension.php
@@ -35,6 +35,7 @@ final class NetgenSyliusBlockManagerExtension extends Extension implements Prepe
     {
         $prependConfigs = array(
             'liip_imagine.yml' => 'liip_imagine',
+            'design.yml' => 'netgen_block_manager',
             'value_types.yml' => 'netgen_block_manager',
             'query_types.yml' => 'netgen_block_manager',
             'view/item_view.yml' => 'netgen_block_manager',
