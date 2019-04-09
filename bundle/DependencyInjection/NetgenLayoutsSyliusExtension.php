5d8f07bb847bb9b313baeea96dcade6977f7a47f
Edi Modrić <edi.modric@gmail.com>
2019-04-09 09:55:30 +0200

    Load services with glob patterns

--- bundle/DependencyInjection/NetgenLayoutsSyliusExtension.php
+++ bundle/DependencyInjection/NetgenLayoutsSyliusExtension.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 namespace Netgen\Bundle\LayoutsSyliusBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Loader\DelegatingLoader;
+use Symfony\Component\Config\Loader\LoaderResolver;
 use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader\GlobFileLoader;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Yaml\Yaml;
@@ -16,20 +19,18 @@ final class NetgenLayoutsSyliusExtension extends Extension implements PrependExt
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new YamlFileLoader(
-            $container,
-            new FileLocator(__DIR__ . '/../Resources/config')
+        $locator = new FileLocator(__DIR__ . '/../Resources/config');
+
+        $loader = new DelegatingLoader(
+            new LoaderResolver(
+                [
+                    new GlobFileLoader($container, $locator),
+                    new YamlFileLoader($container, $locator),
+                ]
+            )
         );
 
-        $loader->load('services/items.yml');
-        $loader->load('services/security.yml');
-        $loader->load('services/event_listeners.yml');
-        $loader->load('services/layout_resolver.yml');
-        $loader->load('services/validators.yml');
-        $loader->load('services/templating.yml');
-        $loader->load('services/query_types.yml');
-        $loader->load('services/parameters.yml');
-        $loader->load('services/locale.yml');
+        $loader->load('services/**/*.yml', 'glob');
         $loader->load('default_settings.yml');
     }
 
