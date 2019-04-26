a261f1b312cc7e18ac48e2c5ff19775672f60947
Edi Modrić <edi.modric@gmail.com>
2019-04-26 15:33:50 +0200

    Add BC layer for deprecated LocaleBundle in Symfony 4.3

--- lib/Locale/LocaleProvider.php
+++ lib/Locale/LocaleProvider.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 namespace Netgen\Layouts\Sylius\Locale;
 
 use Netgen\BlockManager\Locale\LocaleProviderInterface;
+use Netgen\BlockManager\Utils\BackwardsCompatibility\Locales;
 use Sylius\Component\Locale\Provider\LocaleProviderInterface as SyliusLocaleProviderInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Intl\Intl;
 
 /**
  * Sylius specific locale provider that provides the existing locales
@@ -20,15 +20,9 @@ final class LocaleProvider implements LocaleProviderInterface
      */
     private $syliusLocaleProvider;
 
-    /**
-     * @var \Symfony\Component\Intl\ResourceBundle\LocaleBundleInterface
-     */
-    private $localeBundle;
-
     public function __construct(SyliusLocaleProviderInterface $syliusLocaleProvider)
     {
         $this->syliusLocaleProvider = $syliusLocaleProvider;
-        $this->localeBundle = Intl::getLocaleBundle();
     }
 
     public function getAvailableLocales(): array
@@ -36,11 +30,11 @@ final class LocaleProvider implements LocaleProviderInterface
         $availableLocales = [];
 
         foreach ($this->syliusLocaleProvider->getAvailableLocalesCodes() as $localeCode) {
-            $localeName = $this->localeBundle->getLocaleName($localeCode);
-
-            if ($localeName !== null) {
-                $availableLocales[$localeCode] = $localeName;
+            if (!Locales::exists($localeCode)) {
+                continue;
             }
+
+            $availableLocales[$localeCode] = Locales::getName($localeCode);
         }
 
         asort($availableLocales);
