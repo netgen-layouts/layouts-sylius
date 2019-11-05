a08f769732e301557fdf287659862c1fda56bb4d
Edi Modrić <edi.modric@gmail.com>
2019-11-05 15:44:51 +0100

    Fix usage of deprecated Doctrine\DBAL\Types\Type class

--- lib/Layout/Resolver/TargetHandler/Doctrine/Product.php
+++ lib/Layout/Resolver/TargetHandler/Doctrine/Product.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Netgen\Layouts\Sylius\Layout\Resolver\TargetHandler\Doctrine;
 
 use Doctrine\DBAL\Query\QueryBuilder;
-use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Types\Types;
 use Netgen\Layouts\Persistence\Doctrine\QueryHandler\TargetHandlerInterface;
 
 final class Product implements TargetHandlerInterface
@@ -15,6 +15,6 @@ final class Product implements TargetHandlerInterface
         $query->andWhere(
             $query->expr()->eq('rt.value', ':target_value')
         )
-        ->setParameter('target_value', $value, Type::INTEGER);
+        ->setParameter('target_value', $value, Types::INTEGER);
     }
 }
