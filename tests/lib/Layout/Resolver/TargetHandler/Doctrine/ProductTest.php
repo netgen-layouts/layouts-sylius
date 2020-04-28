fe60d67c34fa31518046cd89ac7537e40a667299
Edi Modrić <edi.modric@gmail.com>
2020-04-28 12:25:03 +0200

    Corect path to fixtures

--- tests/lib/Layout/Resolver/TargetHandler/Doctrine/ProductTest.php
+++ tests/lib/Layout/Resolver/TargetHandler/Doctrine/ProductTest.php
@@ -33,6 +33,6 @@ final class ProductTest extends AbstractTargetHandlerTest
 
     protected function insertDatabaseFixtures(string $fixturesPath): void
     {
-        parent::insertDatabaseFixtures(__DIR__ . '/../../../../../_fixtures');
+        parent::insertDatabaseFixtures(__DIR__ . '/../../../../../_fixtures/data.php');
     }
 }
--- tests/lib/Layout/Resolver/TargetHandler/Doctrine/TaxonTest.php
+++ tests/lib/Layout/Resolver/TargetHandler/Doctrine/TaxonTest.php
@@ -33,6 +33,6 @@ final class TaxonTest extends AbstractTargetHandlerTest
 
     protected function insertDatabaseFixtures(string $fixturesPath): void
     {
-        parent::insertDatabaseFixtures(__DIR__ . '/../../../../../_fixtures');
+        parent::insertDatabaseFixtures(__DIR__ . '/../../../../../_fixtures/data.php');
     }
 }
