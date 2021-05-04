b6d0aae9df37e32d6efd9f408ddc010eb9413f1f
Edi Modrić <edi.modric@gmail.com>
2021-05-04 15:45:59 +0200

    Upgrade PHP CS Fixer to v3

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -3,7 +3,7 @@
 // To support running PHP CS Fixer via PHAR file (e.g. in GitHub Actions)
 require_once __DIR__ . '/vendor/netgen/layouts-coding-standard/lib/PhpCsFixer/Config.php';
 
-return Netgen\Layouts\CodingStandard\PhpCsFixer\Config::create()
+return (new Netgen\Layouts\CodingStandard\PhpCsFixer\Config())
     ->setFinder(
         PhpCsFixer\Finder::create()
             ->exclude(['vendor', 'node_modules'])
