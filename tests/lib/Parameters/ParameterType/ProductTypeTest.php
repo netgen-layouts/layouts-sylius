5216b78364620bcf3c63a51ba150853b4b05af6d
Randy Čupić <randy@netgen.hr>
2021-07-07 11:18:18 +0200

    LAYOUTS-619 Implement tests for Sylius product parameter type

---
+++ tests/lib/Parameters/ParameterType/ProductTypeTest.php
@@ -0,0 +1,178 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Layouts\Sylius\Tests\Parameters\ParameterType;
+
+use Netgen\Layouts\Parameters\ParameterDefinition;
+use Netgen\Layouts\Sylius\Parameters\ParameterType\ProductType;
+use Netgen\Layouts\Sylius\Tests\Stubs\Product as ProductStub;
+use Netgen\Layouts\Sylius\Tests\Validator\RepositoryValidatorFactory;
+use Netgen\Layouts\Tests\Parameters\ParameterType\ParameterTypeTestTrait;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Sylius\Component\Product\Repository\ProductRepositoryInterface;
+use Symfony\Component\OptionsResolver\Exception\InvalidArgumentException;
+use Symfony\Component\Validator\Validation;
+
+final class ProductTypeTest extends TestCase
+{
+    use ParameterTypeTestTrait;
+
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject&\Sylius\Component\Product\Repository\ProductRepositoryInterface
+     */
+    private MockObject $repositoryMock;
+
+    protected function setUp(): void
+    {
+        $this->repositoryMock = $this->createMock(ProductRepositoryInterface::class);
+
+        $this->type = new ProductType();
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Sylius\Parameters\ParameterType\ProductType::getIdentifier
+     */
+    public function testGetIdentifier(): void
+    {
+        self::assertSame('sylius_product', $this->type::getIdentifier());
+    }
+
+    /**
+     * @param mixed[] $options
+     * @param mixed[] $resolvedOptions
+     *
+     * @covers \Netgen\Layouts\Sylius\Parameters\ParameterType\ProductType::configureOptions
+     * @dataProvider validOptionsDataProvider
+     */
+    public function testValidOptions(array $options, array $resolvedOptions): void
+    {
+        $parameter = $this->getParameterDefinition($options);
+        self::assertSame($resolvedOptions, $parameter->getOptions());
+    }
+
+    /**
+     * @param mixed[] $options
+     *
+     * @covers \Netgen\Layouts\Sylius\Parameters\ParameterType\ProductType::configureOptions
+     * @dataProvider invalidOptionsDataProvider
+     */
+    public function testInvalidOptions(array $options): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $this->getParameterDefinition($options);
+    }
+
+    /**
+     * Provider for testing valid parameter attributes.
+     *
+     * @return mixed[]
+     */
+    public function validOptionsDataProvider(): array
+    {
+        return [
+            [
+                [],
+                [],
+            ],
+        ];
+    }
+
+    /**
+     * Provider for testing invalid parameter attributes.
+     *
+     * @return mixed[]
+     */
+    public function invalidOptionsDataProvider(): array
+    {
+        return [
+            [
+                [
+                    'undefined_value' => 'Value',
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Sylius\Parameters\ParameterType\ProductType::getValueConstraints
+     */
+    public function testValidationValid(): void
+    {
+        $this->repositoryMock
+                ->expects(self::once())
+                ->method('find')
+                ->with(self::identicalTo(42))
+                ->willReturn(new ProductStub(42));
+
+        $parameter = $this->getParameterDefinition([], true);
+        $validator = Validation::createValidatorBuilder()
+            ->setConstraintValidatorFactory(new RepositoryValidatorFactory($this->repositoryMock))
+            ->getValidator();
+
+        $errors = $validator->validate(42, $this->type->getConstraints($parameter, 42));
+        self::assertCount(0, $errors);
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Sylius\Parameters\ParameterType\ProductType::getValueConstraints
+     */
+    public function testValidationValidWithNonRequiredValue(): void
+    {
+        $this->repositoryMock
+                ->expects(self::never())
+                ->method('find');
+
+        $parameter = $this->getParameterDefinition([], false);
+        $validator = Validation::createValidatorBuilder()
+            ->setConstraintValidatorFactory(new RepositoryValidatorFactory($this->repositoryMock))
+            ->getValidator();
+
+        $errors = $validator->validate(null, $this->type->getConstraints($parameter, null));
+        self::assertCount(0, $errors);
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Sylius\Parameters\ParameterType\ProductType::getValueConstraints
+     */
+    public function testValidationInvalid(): void
+    {
+        $this->repositoryMock
+                ->expects(self::once())
+                ->method('find')
+                ->with(self::identicalTo(42))
+                ->willReturn(null);
+
+        $parameter = $this->getParameterDefinition([], true);
+        $validator = Validation::createValidatorBuilder()
+            ->setConstraintValidatorFactory(new RepositoryValidatorFactory($this->repositoryMock))
+            ->getValidator();
+
+        $errors = $validator->validate(42, $this->type->getConstraints($parameter, 42));
+        self::assertNotCount(0, $errors);
+    }
+
+    /**
+     * @param mixed $value
+     *
+     * @covers \Netgen\Layouts\Sylius\Parameters\ParameterType\ProductType::isValueEmpty
+     * @dataProvider emptyDataProvider
+     */
+    public function testIsValueEmpty($value, bool $isEmpty): void
+    {
+        self::assertSame($isEmpty, $this->type->isValueEmpty(new ParameterDefinition(), $value));
+    }
+
+    /**
+     * @return mixed[]
+     */
+    public function emptyDataProvider(): array
+    {
+        return [
+            [null, true],
+            [new ProductStub(42), false],
+        ];
+    }
+}
