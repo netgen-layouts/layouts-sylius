ca0ba896346f85ad658540472bbb105034649b71
Randy Čupić <randy@netgen.hr>
2023-07-20 14:10:11 +0200

    Implement condition type for Sylius resource types

---
+++ lib/Layout/Resolver/Form/ConditionType/Mapper/ResourceType.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Layouts\Sylius\Layout\Resolver\Form\ConditionType\Mapper;
+
+use Netgen\Layouts\Layout\Resolver\Form\ConditionType\Mapper;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+use function str_replace;
+use function ucfirst;
+
+final class ResourceType extends Mapper
+{
+    /**
+     * @param array<string, string> $allowedResources
+     */
+    public function __construct(private readonly array $allowedResources)
+    {
+    }
+
+    public function getFormType(): string
+    {
+        return ChoiceType::class;
+    }
+
+    public function getFormOptions(): array
+    {
+        return [
+            'choices' => $this->getResourceTypeList(),
+            'choice_translation_domain' => false,
+            'multiple' => true,
+            'expanded' => true,
+        ];
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    private function getResourceTypeList(): array
+    {
+        $resourceTypeList = [];
+
+        foreach ($this->allowedResources as $type) {
+            $resourceTypeList[$this->humanizeType($type)] = $type;
+        }
+
+        return $resourceTypeList;
+    }
+
+    private function humanizeType(string $type): string
+    {
+        return ucfirst(str_replace(['-', '_'], ' ', $type));
+    }
+}
