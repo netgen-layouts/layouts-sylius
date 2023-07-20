52a566489b5a8496d59356d331fd094e37f82b2c
Randy Čupić <randy@netgen.hr>
2023-07-20 14:12:17 +0200

    Implement target type for Sylius pages

---
+++ lib/Layout/Resolver/Form/TargetType/Mapper/Page.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Layouts\Sylius\Layout\Resolver\Form\TargetType\Mapper;
+
+use Netgen\Layouts\Layout\Resolver\Form\TargetType\Mapper;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+use function str_replace;
+use function ucfirst;
+
+final class Page extends Mapper
+{
+    /**
+     * @param array<string, string> $allowedPages
+     */
+    public function __construct(private readonly array $allowedPages)
+    {
+    }
+
+    public function getFormType(): string
+    {
+        return ChoiceType::class;
+    }
+
+    public function getFormOptions(): array
+    {
+        return [
+            'choices' => $this->getPagesList(),
+            'choice_translation_domain' => false,
+            'multiple' => false,
+            'expanded' => false,
+        ];
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    private function getPagesList(): array
+    {
+        $pageList = [];
+
+        foreach ($this->allowedPages as $page) {
+            $pageList[$this->humanizePage($page)] = $page;
+        }
+
+        return $pageList;
+    }
+
+    private function humanizePage(string $allowedPages): string
+    {
+        return ucfirst(str_replace(['-', '_'], ' ', $allowedPages));
+    }
+}
---
+++ lib/Layout/Resolver/TargetType/Page.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Layouts\Sylius\Layout\Resolver\TargetType;
+
+use Netgen\Layouts\Layout\Resolver\TargetType;
+use Netgen\Layouts\Sylius\Validator\Constraint as SyliusConstraints;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints;
+
+final class Page extends TargetType
+{
+    /**
+     * @param array<string, string> $availablePages
+     */
+    public function __construct(private readonly array $availablePages)
+    {
+    }
+
+    public static function getType(): string
+    {
+        return 'sylius_page';
+    }
+
+    public function getConstraints(): array
+    {
+        return [
+            new Constraints\NotBlank(),
+            new Constraints\Type(['type' => 'string']),
+            new SyliusConstraints\Page(),
+        ];
+    }
+
+    public function provideValue(Request $request): ?string
+    {
+        $route = $request->attributes->get('_route');
+
+        return $this->availablePages[$route] ?? null;
+    }
+}
