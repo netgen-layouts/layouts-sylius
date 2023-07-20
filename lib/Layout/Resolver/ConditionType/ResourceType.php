e37c9f54871835beec96f376b35d017e10bc6315
Randy Čupić <randy@netgen.hr>
2023-07-20 14:10:11 +0200

    Implement condition type for Sylius resource types

---
+++ lib/Layout/Resolver/ConditionType/ResourceType.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Layouts\Sylius\Layout\Resolver\ConditionType;
+
+use Netgen\Layouts\Layout\Resolver\ConditionType;
+use Netgen\Layouts\Sylius\Validator\Constraint as SyliusConstraints;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints;
+
+use function array_flip;
+use function array_map;
+use function is_a;
+
+final class ResourceType extends ConditionType
+{
+    /**
+     * @param array<string, string> $allowedResources
+     */
+    public function __construct(private readonly array $allowedResources)
+    {
+    }
+
+    public static function getType(): string
+    {
+        return 'sylius_resource_type';
+    }
+
+    public function getConstraints(): array
+    {
+        return [
+            new Constraints\NotBlank(),
+            new Constraints\All(
+                [
+                    'constraints' => [
+                        new Constraints\Type(['type' => 'string']),
+                        new SyliusConstraints\ResourceType(),
+                    ],
+                ],
+            ),
+        ];
+    }
+
+    public function matches(Request $request, mixed $value): bool
+    {
+        $resource = $request->attributes->get('nglayouts_sylius_resource');
+
+        $allowedClasses = array_map(fn (string $value): string => array_flip($this->allowedResources)[$value], $value);
+
+        foreach ($allowedClasses as $allowedClass) {
+            if (is_a($resource, $allowedClass)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
