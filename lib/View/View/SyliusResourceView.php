7dd6e256bbddfdf16061e4c55cd513ec5456d765
Randy Čupić <randy@netgen.hr>
2023-11-01 21:27:24 +0100

    Create view for Sylius resource

---
+++ lib/View/View/SyliusResourceView.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Layouts\Sylius\View\View;
+
+use Netgen\Layouts\View\View;
+use Sylius\Component\Resource\Model\ResourceInterface;
+
+final class SyliusResourceView extends View implements SyliusResourceViewInterface
+{
+    public function __construct(ResourceInterface $resource, string $viewType)
+    {
+        $this->parameters['resource'] = $resource;
+        $this->parameters['view_type'] = $viewType;
+    }
+
+    public function getResource(): ResourceInterface
+    {
+        return $this->parameters['resource'];
+    }
+
+    public function getViewType(): string
+    {
+        return $this->parameters['view_type'];
+    }
+
+    public static function getIdentifier(): string
+    {
+        return 'sylius_resource';
+    }
+}
---
+++ lib/View/View/SyliusResourceViewInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Layouts\Sylius\View\View;
+
+use Netgen\Layouts\View\ViewInterface;
+use Sylius\Component\Resource\Model\ResourceInterface;
+
+interface SyliusResourceViewInterface extends ViewInterface
+{
+    /**
+     * Returns the Sylius resource.
+     */
+    public function getResource(): ResourceInterface;
+
+    /**
+     * Returns the view type with which the Sylius resource will be rendered.
+     */
+    public function getViewType(): string;
+}
